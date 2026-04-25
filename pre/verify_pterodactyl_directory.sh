b50d5ff668e7c898e4aea90e6cc6822866f8bf23
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-04-25 15:22:47 +0530

    update directory checks

--- pre/verify_pterodactyl_directory.sh
+++ pre/verify_pterodactyl_directory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REQUIRED_DIRS=("app" "public" "routes")
-REQUIRED_FILES=("tailwind.config.js")
+REQUIRED_FILES=("artisan")
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 BASE_DIR="$(dirname "$SCRIPT_DIR")"
 
@@ -9,14 +9,14 @@ source "$BASE_DIR/lib/core.sh"
 
 for dir in "${REQUIRED_DIRS[@]}"; do
     if [ ! -d "$dir" ]; then
-        echo -e "$WARN You cannot run this command outside of Pterodactyl directory."
+        echo -e "$WARN You cannot run this command outside of Panel directory."
         exit 1
     fi
 done
 
 for file in "${REQUIRED_FILES[@]}"; do
     if [ ! -f "$file" ]; then
-        echo -e "$WARN You cannot run this command outside of Pterodactyl directory."
+        echo -e "$WARN You cannot run this command outside of Panel directory."
         exit 1
     fi
 done
